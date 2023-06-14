2337b4412ce52345e303ac404e68e54b12766b27
acaValkyrie <acavalkyrie@gmail.com>
2023-06-14 18:09:40 +0900

    make WrapCoodinatesInRange func

--- Assets/Scripts/PlayerLimitter.cs
+++ Assets/Scripts/PlayerLimitter.cs
@@ -6,60 +6,73 @@ public class PlayerLimitter : MonoBehaviour
 {
     class Wall
     {
-        private GameObject up;
-        private GameObject down;
+        private GameObject back;
+        private GameObject front;
         private GameObject right;
         private GameObject left;
 
-        public float upLimit;
-        public float downLimit;
-        public float rightLimit;
-        public float leftLimit;
+        public float zMax;
+        public float zMin;
+        public float xMax;
+        public float xMin;
 
-        public Wall(GameObject upObj, GameObject downObj, GameObject rightObj, GameObject leftObj)
+        public Wall(GameObject backObj, GameObject frontObj, GameObject rightObj, GameObject leftObj)
         {
-            this.up = upObj;
-            this.down = downObj;
+            this.back = backObj;
+            this.front = frontObj;
             this.right = rightObj;
             this.left = leftObj;
-            this.upLimit = up.transform.position.z;
-            this.downLimit = down.transform.position.z;
-            this.rightLimit = right.transform.position.x;
-            this.leftLimit = left.transform.position.x;
+            this.zMax = back.transform.position.z;
+            this.zMin = front.transform.position.z;
+            this.xMax = right.transform.position.x;
+            this.xMin = left.transform.position.x;
         }
     }
-    [SerializeField] private GameObject upWall;
-    [SerializeField] private GameObject downWall;
+    [SerializeField] private GameObject backWall;
+    [SerializeField] private GameObject frontWall;
     [SerializeField] private GameObject rightWall;
     [SerializeField] private GameObject leftWall;
     private Wall _wall;
     
+    private Camera _camera;
+    private float _cameraHeight;
+    [SerializeField] private GameObject floor;
+    private float _floorHeight;
+
     [SerializeField] private GameObject player;
     
-    // Start is called before the first frame update
+    // Start is called before the first frame backdate
     void Start()
     {
-        _wall = new Wall(upWall, downWall, rightWall, leftWall);
+        _wall = new Wall(backWall, frontWall, rightWall, leftWall);
+        _camera = Camera.main;
+        _cameraHeight = _camera.transform.position.y;
+        _floorHeight = floor.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        WrapCoordinatesInRange();
+    }
+
+    void WrapCoordinatesInRange()
+    {
         Vector3 playerPos = player.transform.position;
-        if (playerPos.z < _wall.upLimit && playerPos.z > _wall.downLimit && playerPos.x < _wall.rightLimit && playerPos.x > _wall.leftLimit && playerPos.y < 10.0f && playerPos.y > 0.0f)
+        // if all in range, do nothing
+        if (playerPos.z < _wall.zMax    && playerPos.z > _wall.zMin && 
+            playerPos.x < _wall.xMax    && playerPos.x > _wall.xMin && 
+            playerPos.y < _cameraHeight && playerPos.y > _floorHeight)
             return;
-        if (playerPos.z > _wall.upLimit)
-            playerPos.z = _wall.downLimit;
-        if (playerPos.z < _wall.downLimit)
-            playerPos.z = _wall.upLimit;
-        if (playerPos.x > _wall.rightLimit)
-            playerPos.x = _wall.leftLimit;
-        if(playerPos.x < _wall.leftLimit)
-            playerPos.x = _wall.rightLimit;
-        if(playerPos.y < 0.0f)
-            playerPos.y = 10.0f;
-        if(playerPos.y > 10.0f)
-            playerPos.y = 0.0f;
+        WrapInRange(ref playerPos.z, _wall.zMin, _wall.zMax);
+        WrapInRange(ref playerPos.x, _wall.xMin, _wall.xMax);
+        WrapInRange(ref playerPos.y, _floorHeight, _cameraHeight);
         player.transform.position = playerPos;
+    }
+
+    void WrapInRange(ref float num, float min, float max)
+    {
+        if (num < min) num = max;
+        if (num > max) num = min;
     }
 }
