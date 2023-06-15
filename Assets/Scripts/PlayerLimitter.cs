5aded13e1fa8f87dde20bdb0e94c82fe4d432325
acaValkyrie <acavalkyrie@gmail.com>
2023-06-15 14:07:23 +0900

    change Wall class to Box class

--- Assets/Scripts/PlayerLimitter.cs
+++ Assets/Scripts/PlayerLimitter.cs
@@ -4,50 +4,43 @@ using UnityEngine;
 
 public class PlayerLimitter : MonoBehaviour
 {
-    class Wall
+    class Box
     {
-        private GameObject back;
-        private GameObject front;
-        private GameObject right;
-        private GameObject left;
-
         public float zMax;
         public float zMin;
         public float xMax;
         public float xMin;
+        public float yMax;
+        public float yMin;
 
-        public Wall(GameObject backObj, GameObject frontObj, GameObject rightObj, GameObject leftObj)
+        public Box(GameObject backObj,  GameObject frontObj,
+                   GameObject rightObj, GameObject leftObj, 
+                   GameObject topObj,   GameObject bottomObj)
         {
-            this.back = backObj;
-            this.front = frontObj;
-            this.right = rightObj;
-            this.left = leftObj;
-            this.zMax = back.transform.position.z;
-            this.zMin = front.transform.position.z;
-            this.xMax = right.transform.position.x;
-            this.xMin = left.transform.position.x;
+            this.zMax = backObj.transform.position.z;
+            this.zMin = frontObj.transform.position.z;
+            this.xMax = rightObj.transform.position.x;
+            this.xMin = leftObj.transform.position.x;
+            this.yMax = topObj.transform.position.y;
+            this.yMin = bottomObj.transform.position.y;
         }
     }
     [SerializeField] private GameObject backWall;
     [SerializeField] private GameObject frontWall;
     [SerializeField] private GameObject rightWall;
     [SerializeField] private GameObject leftWall;
-    private Wall _wall;
+    private Box _box;
     
     private Camera _camera;
-    private float _cameraHeight;
     [SerializeField] private GameObject floor;
-    private float _floorHeight;
 
     [SerializeField] private GameObject player;
     
     // Start is called before the first frame backdate
     void Start()
     {
-        _wall = new Wall(backWall, frontWall, rightWall, leftWall);
         _camera = Camera.main;
-        _cameraHeight = _camera.transform.position.y;
-        _floorHeight = floor.transform.position.y;
+        _box = new Box(backWall, frontWall, rightWall, leftWall, _camera.gameObject, floor);
     }
 
     // Update is called once per frame
@@ -60,13 +53,13 @@ public class PlayerLimitter : MonoBehaviour
     {
         Vector3 playerPos = player.transform.position;
         // if all in range, do nothing
-        if (playerPos.z < _wall.zMax    && playerPos.z > _wall.zMin && 
-            playerPos.x < _wall.xMax    && playerPos.x > _wall.xMin && 
-            playerPos.y < _cameraHeight && playerPos.y > _floorHeight)
+        if (playerPos.z < _box.zMax && playerPos.z > _box.zMin && 
+            playerPos.x < _box.xMax && playerPos.x > _box.xMin && 
+            playerPos.y < _box.yMax && playerPos.y > _box.yMin)
             return;
-        WrapInRange(ref playerPos.z, _wall.zMin, _wall.zMax);
-        WrapInRange(ref playerPos.x, _wall.xMin, _wall.xMax);
-        WrapInRange(ref playerPos.y, _floorHeight, _cameraHeight);
+        WrapInRange(ref playerPos.z, _box.zMin, _box.zMax);
+        WrapInRange(ref playerPos.x, _box.xMin, _box.xMax);
+        WrapInRange(ref playerPos.y, _box.yMin, _box.yMax);
         player.transform.position = playerPos;
     }
 
